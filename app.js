19a2bf5c149b1a2adbbac51f4630e2131fe08359
dayfine <difancpa@gmail.com>
2017-07-11 21:48:46 -0400

    trying render

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const nunjucks = require('nunjucks');
-const app = express();
+const express = require('express')
+const nunjucks = require('nunjucks')
+const app = express()
 
-app.use(function(req, res, next){
-  console.log(req.url, 'url');
-  console.log(req.body, 'body');
-//needs more work on logging
-  next();
-});
-app.get('/', function(req, res, next){
-  res.send('Hello World');
-});
+app.use(function (req, res, next) {
+  console.log(req.url, 'url')
+  console.log(req.body, 'body')
+  // needs more work on logging
+  next()
+})
 
+app.set('view engine', 'html')
+app.engine('html', nunjucks.render)
+nunjucks.configure('views', {noCache: true})
 
+var locals = {
+  title: 'An Example',
+  people: [ { name: 'Gandalf'}, { name: 'Frodo' }, { name: 'Hermione'} ]
+}
 
-app.listen(3000, function(){
-  console.log('server listening');
-});
+app.get('/', function (req, res, next) {
+  res.render('index', locals, function (err, output) {
+    console.log(output)
+  })
+  next()
+})
+
+app.listen(3000, function () {
+  console.log('server listening')
+})
