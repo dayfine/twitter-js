397f5eb36a5ee67505eeae1d945de0fadf238116
dayfine <difancpa@gmail.com>
2017-07-12 20:38:43 -0400

    routring done

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,36 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 // could use one line instead: const router = require('express').Router();
-const tweetBank = require('../tweetBank');
+const tweetBank = require('../tweetBank')
 
 router.get('/', function (req, res) {
-  let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
-});
-
-router.get('/stylesheets/style.css', function(req, res){
-  res.sendFile('/public/stylesheets/style.css');
+  let tweets = tweetBank.list()
+  res.render('index', { tweets: tweets })
 })
+
+router.get('/stylesheets/style.css', function (req, res) {
+  res.sendFile('/public/stylesheets/style.css')
+})
+
 // extra credit
-//For every incoming request:
+// For every incoming request:
 
 // Use request.path to get the route
 // See if that route maps to a valid file in the public directory
 // If not, go defer to the next matching middleware
 // If the file matches, send over its contents
 
-module.exports = router;
+router.get('/users/:name', function (req, res) {
+  // make sure to match cases
+  var name = req.params.name
+  var tweets = tweetBank.find({name: name})
+  res.render('index', { tweets: tweets })
+})
+
+router.get('/tweets/:id', function (req, res) {
+  var id = +req.params.id
+  var tweets = tweetBank.find({id: id})
+  res.render('index', { tweets: tweets })
+})
+
+module.exports = router
